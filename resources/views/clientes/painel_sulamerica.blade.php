0418c7797b65218a3c25caad0d460aa2011dedbc
fqueta <ger.maisaqui1@gmail.com>
2026-03-10 14:44:25 -0300

    10/03/2026

--- resources/views/clientes/painel_sulamerica.blade.php
+++ resources/views/clientes/painel_sulamerica.blade.php
@@ -26,13 +26,13 @@
                 </button>
             </div>
     @elseif($status_contrato == 'Cancelado' || $status_contrato == 'cancelado')
-        @if ($dados)
+        {{-- @if ($dados)
 
             <div class="col-md-12 text-right">
                 <button title="{{__('Reativar cadastro')}}" onclick="reativar_cadastro('{{$token}}','{{ URL::full() }}')" type="button" class="btn btn-outline-secondary ml-1"><i class="fa fa-recycle"></i> {{ __('Reativar sulamerica') }}</button>
             </div>
 
-        @endif
+        @endif --}}
         <div class="col-md-12 text-right">
                 <button type="button" title="{{__('Cantelar o contrato na sulamerica')}}" data-operacao="{{$numOperacao}}" onclick="cancelarSulamerica('{{$token}}','{{$id}}',this)" class="btn btn-outline-danger">
                    <i class="fa fa-ban"></i> {{__('Cancelar novamente')}}
